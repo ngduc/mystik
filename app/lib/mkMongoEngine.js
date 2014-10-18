f06eaa3cbf0fdda3b9df0626a5b83c743a8181a0
ngduc <ducjava@gmail.com>
2014-10-18 10:52:14 -0700

    added options for query methods; support sort option for MongoEngine;

--- app/lib/mkMongoEngine.js
+++ app/lib/mkMongoEngine.js
@@ -1,42 +1,49 @@
 /* jshint -W030 */
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(['./mkUtils'], function (Utils) {
+define( ['./mkUtils'], function ( Utils ) {
     // DB client: https://github.com/learnboost/mongoose
 
-    var MkMongoEngine = function (client) {
+    var MkMongoEngine = function ( client ) {
         var DEBUG = true;
         var _client = client;
 
+        var processOptions = function( opts ) {
+            return {
+                sort: ( opts && opts.sort ? opts.sort : {} )
+            }
+        }
+
         return {
-            find: function (table, params, callback) {
+            find: function ( table, params, callback, options ) {
+                var doneFn = ( callback ? callback : Utils.defer() );
+                var Table = _client.model( table ),
+                    options = processOptions( options );
+
+                Table.find( params ).setOptions( { lean: true } ).sort( options.sort ).exec( function ( err, res ) {
+                    Utils.done( doneFn, Utils.wrapError( err ), Utils.wrapResult( res, res ) );
+                } );
+                if ( doneFn && doneFn.promise ) return doneFn.promise;
+            },
+            findOne: function ( table, params, callback ) {
                 var doneFn = ( callback ? callback : Utils.defer() );
                 var Table = _client.model( table );
 
-                Table.find( params, function( err, res ) {
+                Table.findOne( params, function ( err, res ) {
                     Utils.done( doneFn, Utils.wrapError( err ), Utils.wrapResult( res, res ) );
-                });
+                } );
                 if ( doneFn && doneFn.promise ) return doneFn.promise;
             },
-            findOne: function (table, params, callback) {
-                var doneFn = ( callback ? callback : Utils.defer() );
-                var Table = _client.model( table );
-
-                Table.findOne( params, function( err, res ) {
-                    Utils.done( doneFn, Utils.wrapError( err ), Utils.wrapResult( res, res ) );
-                });
-                if ( doneFn && doneFn.promise ) return doneFn.promise;
-            },
-            findWhere: function (table, whereClause, params, callback) {
+            findWhere: function ( table, whereClause, params, callback ) {
                 return null; // not implemented
             },
-            findOneWhere: function (table, whereClause, params, callback) {
+            findOneWhere: function ( table, whereClause, params, callback ) {
                 return null; // not implemented
             },
-            findAll: function (table, callback) {
-                return this.find( table, {}, callback);
+            findAll: function ( table, callback ) {
+                return this.find( table, {}, callback );
             },
-            insert: function (table, obj, callback) {
+            insert: function ( table, obj, callback ) {
                 var doneFn = ( callback ? callback : Utils.defer() );
                 var Table = _client.model( table );
                 var item = new Table(), k;
@@ -52,36 +59,36 @@ define(['./mkUtils'], function (Utils) {
                 var doneFn = ( callback ? callback : Utils.defer() );
                 var Table = _client.model( table );
 
-                Table.findOne( {}, params, function( err, res ) {
+                Table.findOne( {}, params, function ( err, res ) {
                     for ( var k in obj ) {
                         res[ k ] = obj[ k ];
                     }
                     res.save( function ( err, res ) {
                         Utils.done( doneFn, Utils.wrapError( err ), Utils.wrapResult( res, res ) );
                     } );
-                });
+                } );
                 if ( doneFn && doneFn.promise ) return doneFn.promise;
             },
-            count: function (table, callback) {
+            count: function ( table, callback ) {
                 var doneFn = ( callback ? callback : Utils.defer() );
                 var Table = _client.model( table );
 
-                Table.count( {}, function( err, res ) {
+                Table.count( {}, function ( err, res ) {
                     Utils.done( doneFn, Utils.wrapError( err ), Utils.wrapResult( res, res ) );
-                });
+                } );
                 if ( doneFn && doneFn.promise ) return doneFn.promise;
             },
-            delete: function (table, params, callback) {
+            delete: function ( table, params, callback ) {
                 var doneFn = ( callback ? callback : Utils.defer() );
                 var Table = _client.model( table );
 
-                Table.remove( params, function( err, res ) {
+                Table.remove( params, function ( err, res ) {
                     Utils.done( doneFn, err, res );
-                });
+                } );
                 if ( doneFn && doneFn.promise ) return doneFn.promise;
             }
         };
     };
 
     return MkMongoEngine;
-});
+} );
--- app/lib/mkTable.js
+++ app/lib/mkTable.js
@@ -1,50 +1,50 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define([], function() {
+define( [], function () {
 
     // TODO:
     // - handle Error/Exceptions
 
-    var MkTable = function(engine, table, validator) {
+    var MkTable = function ( engine, table, validator ) {
         var _engine = engine,
             _table = table,
             _validate = validator;
 
         return {
-            find: function(params, callback) {
-                if (_validate) {
-                    var errObj = _validate.find(params);
-                    if (callback) callback(errObj, null);
+            find: function ( params, callback, options ) {
+                if ( _validate ) {
+                    var errObj = _validate.find( params );
+                    if ( callback ) callback( errObj, null );
                     return;
                 }
-                return _engine.find(_table, params, callback);
+                return _engine.find( _table, params, callback, options );
             },
-            findOne: function(params, callback) {
-                return _engine.findOne(_table, params, callback);
+            findOne: function ( params, callback, options ) {
+                return _engine.findOne( _table, params, callback, options );
             },
-            findWhere: function(sql, params, callback) {
-                return _engine.findWhere(_table, sql, params, callback);
+            findWhere: function ( sql, params, callback, options ) {
+                return _engine.findWhere( _table, sql, params, callback, options );
             },
-            findOneWhere: function(sql, params, callback) {
-                return _engine.findOneWhere(_table, sql, params, callback);
+            findOneWhere: function ( sql, params, callback, options ) {
+                return _engine.findOneWhere( _table, sql, params, callback, options );
             },
-            findAll: function(callback) {
-                return _engine.findAll(_table, callback);
+            findAll: function ( callback, options ) {
+                return _engine.findAll( _table, callback, options );
             },
-            insert: function(obj, callback) {
-                return _engine.insert(_table, obj, callback);
+            insert: function ( obj, callback, options ) {
+                return _engine.insert( _table, obj, callback, options );
             },
-            update: function(obj, params, callback) {
-                return _engine.update(_table, obj, params, callback);
+            update: function ( obj, params, callback, options ) {
+                return _engine.update( _table, obj, params, callback, options );
             },
-            count: function(callback) {
-                return _engine.count(_table, callback);
+            count: function ( callback, options ) {
+                return _engine.count( _table, callback, options );
             },
-            delete: function(params, callback) {
-                return _engine.delete(_table, params, callback);
+            delete: function ( params, callback, options ) {
+                return _engine.delete( _table, params, callback, options );
             }
         };
     };
 
     return MkTable;
-});
+} );
